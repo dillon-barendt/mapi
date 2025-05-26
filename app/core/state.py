db4ea63ec47f3c65d8c374f0f6b147a06e0ea3fe
Dillon <dillon.barendt@ticket-vision.com>
2025-05-25 17:59:32 -0700

    Add GitHub Actions for formatting and CI checks
    Introduced two workflows: one for code formatting validation using Black and isort, and another for continuous integration with test execution using pytest. These workflows ensure code quality and prevent formatting or test regressions on pushes and pull requests to the main branch.

--- app/core/state.py
+++ app/core/state.py
@@ -1,12 +1,28 @@
+"""ASGI State Management for the startup, lifespan, and shutdown of the FastAPI application.
+
+ASGI Specification: https://asgi.readthedocs.io/en/latest/specs/main.html
+ASGI: Lifespan Protocol: https://asgi.readthedocs.io/en/latest/specs/lifespan.html
+"""
 from contextlib import asynccontextmanager
 from fastapi.applications import FastAPI
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
     """
-    Lifespan context manager for the FastAPI application.
-    Set up application state, such as precomputing the OpenAPI schema
-    and initializing other global components.
+    Manages the lifespan of the FastAPI application. This asynchronous
+    context manager handles necessary setup at the startup and cleanup
+    at the shutdown of the application, ensuring proper management of
+    application state such as the OpenAPI schema cache and any other
+    resources required for the application's lifecycle.
+
+    Startup tasks can be performed before entering the context scope,
+    while shutdown tasks are executed upon exiting. It facilitates a
+    clean and efficient lifecycle management for the app.
+
+    :param app: The FastAPI application instance whose lifespan is being
+        managed.
+    :type app: FastAPI
+    :return: An asynchronous generator managing the application lifespan.
     """
     # Perform startup tasks here
     app.state.openapi_schema = None  # Initialize the schema cache
